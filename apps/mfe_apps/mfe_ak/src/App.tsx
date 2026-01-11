f6183d929fdd5360b3331bad8b638b935f22b22e
Amandeep Kirar <ak4wrapp@gmail.com>
2026-01-10 22:46:18 -0500

    ak updates

--- apps/mfe_apps/mfe_ak/src/App.tsx
+++ apps/mfe_apps/mfe_ak/src/App.tsx
@@ -1,3 +1,37 @@
 export default function AKApp() {
-  return <h2>Hello from AK!!</h2>;
+  return (
+    <>
+      <h2>Hello from AK!!</h2>
+      <h4>My Projects</h4>
+      <ul>
+        <li>
+          <a
+            href="https://ak-react-concepts-made-easy.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Concepts Made Easy
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://metadata-driven-dynamic-forms.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Metadata Driven Dynamic Forms
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://mfe-monorepo-with-pnpm-shell.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            MFE Monorepo with PNPM
+          </a>
+        </li>
+      </ul>
+    </>
+  );
 }
