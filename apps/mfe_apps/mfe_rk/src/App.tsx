c099e0e0e4556e33f599dd9d3a99dc5712f40482
Amandeep Kirar <ak4wrapp@gmail.com>
2026-01-10 22:42:08 -0500

    read a story

--- apps/mfe_apps/mfe_rk/src/App.tsx
+++ apps/mfe_apps/mfe_rk/src/App.tsx
@@ -1,3 +1,17 @@
 export default function mfe_rk() {
-  return <h2>Hello from RK!! why the dog here !!!!</h2>;
+  return (
+    <>
+      <h2>Hello from RK!! why the dog here !!!!</h2>;<pre>Read a story?</pre>
+      <h4>
+        click{" "}
+        <a
+          href="https://react-concepts-made-easy.vercel.app/learning/excercise/random-story-generator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here
+        </a>
+      </h4>
+    </>
+  );
 }
